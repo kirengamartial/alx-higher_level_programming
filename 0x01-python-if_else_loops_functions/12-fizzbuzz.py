115bccd06988300c7f0caa555f2555799829e0f9
kirengamartial <kirengamartial@gmail.com>
2023-11-28 09:31:41 -0800

    12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
+    """Print numbers from 1 to 100, replacing multiples of 3 and 5."""
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
