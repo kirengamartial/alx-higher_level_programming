b90ab11b4ababee6750eee5230c455e3d64c35e3
kirengamartial <kirengamartial@gmail.com>
2023-11-28 08:33:26 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
-for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)), end="")
-        
+
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['e', 'q']:
+        print("{}".format(chr(char)), end="")
