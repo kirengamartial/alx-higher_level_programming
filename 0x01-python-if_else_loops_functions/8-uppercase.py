06e8cd07c728338b330a1b2871939bd39ee6f15f
kirengamartial <kirengamartial@gmail.com>
2023-11-28 09:06:02 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,12 +3,14 @@
 def uppercase(str):
     """Print a string in uppercase followed by a new line."""
     for char in str:
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end="")
-        else:
-            print(char, end="")
+        upper_char = char
+        if 'a' <= char <= 'z':
+            upper_char = chr(ord(char) - ord('a') + ord('A'))
+        print("{}".format(upper_char), end="")
     print()
 
 if __name__ == "__main__":
+    uppercase = __import__('8-uppercase').uppercase
+
     uppercase("best")
     uppercase("Best School 98 Battery street")
