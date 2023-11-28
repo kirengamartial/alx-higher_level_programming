caca318ae95b876590acb24e50b07c2b44170b6a
kirengamartial <kirengamartial@gmail.com>
2023-11-28 09:25:00 -0800

    11-pow.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Compute a to the power of b."""
+    return a ** b
+
+
+if __name__ == "__main__":
+    pow = __import__('11-pow').pow
+
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5))
